6db78d244bdf78e9c506b235aee63d16142f7321
Chris Sita <cs.sita@me.com>
2018-06-29 12:17:37 -0700

    Inital Commit

---
+++ js/general.js
@@ -0,0 +1,11 @@
+
+//mobile menu
+$(document).ready(function() {
+    $('a#mobile-nav').click(function() {
+        $('.menu').slideToggle('fast');
+        return false;
+         
+    });
+
+
+});
